d11501fd9f939601f034b3856ca669977affc7fe
god <164798848@qq.com>
2017-06-04 23:05:16 +0800

    code

--- addons/install.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-kubectl create -f *.yaml
-kubectl replace -f *.yaml
--- master/up.sh
+++ master/up.sh
@@ -1,8 +1,27 @@
 #!/usr/bin/env bash
 
-#kubelet cert
-openssl genrsa -out kubelet.key 2048
-openssl req -new -key kubelet.key -out kubelet.csr -subj "/CN=kubelet"
-openssl x509 -req -in kubelet.csr -CA ca.crt -CAkey ca.key -CAcreateserial -out kubelet.crt -days 10000
+# 主机名将用于node名称
+vim /etc/hostname
+# 主机名对应的网卡地址
+vim /etc/hosts
 
-sudo ./kubelet --require-kubeconfig true --kubeconfig=../master/kubeconfig.yaml --pod-manifest-path=../master/manifests
+# 重置master或者node
+sudo kubeadm reset
+
+# 启动master
+sudo kubeadm init --apiserver-advertise-address=192.168.99.201 --pod-network-cidr=10.244.0.0/16
+# 添加环境KUBECONFIG变量使用kubectl
+# 复制admin.conf到其他机器使用kubectl。执行kubectl proxy，启动代理
+
+# 安装网络插件
+kubectl apply -f https://raw.githubusercontent.com/weaveworks/weave/master/prog/weave-kube/weave-daemonset-k8s-1.6.yaml
+kubectl apply -f https://raw.githubusercontent.com/romana/romana/master/containerize/specs/romana-kubeadm.yml
+kubectl apply -f http://docs.projectcalico.org/v2.1/getting-started/kubernetes/installation/hosted/kubeadm/1.6/calico.yaml
+
+# 启动node
+kubeadm join --token 961e82.be5b0cf15e566aec 192.168.99.201:6443
+
+# 安装ui插件
+kubectl create -f https://git.io/kube-dashboard
+kubectl proxy
+http://localhost:8001/ui
