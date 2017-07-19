6c0a3810bdca1d0fbd467d7b0fe87773528eef48
god <164798848@qq.com>
2017-07-19 09:09:34 +0800

    code

--- k8s.sh
+++ k8s.sh
@@ -13,6 +13,9 @@ sudo kubeadm init --apiserver-advertise-address=192.168.99.201 --pod-network-cid
 # 添加环境KUBECONFIG变量使用kubectl
 # 复制admin.conf到其他机器使用kubectl。执行kubectl proxy，启动代理
 
+# 查看集群信息
+kubectl cluster-info
+
 # 安装网络插件
 kubectl apply -f https://raw.githubusercontent.com/weaveworks/weave/master/prog/weave-kube/weave-daemonset-k8s-1.6.yaml
 kubectl apply -f https://raw.githubusercontent.com/romana/romana/master/containerize/specs/romana-kubeadm.yml
@@ -25,3 +28,6 @@ kubeadm join --token 961e82.be5b0cf15e566aec 192.168.99.201:6443
 kubectl create -f https://git.io/kube-dashboard
 kubectl proxy
 http://localhost:8001/ui
+
+# 系统资源监控
+kubectl create -f https://raw.githubusercontent.com/luxas/heapster/1308dd71f0ba343895456b46d1bbf3238800b6f3/deploy/kube-config/rbac/heapster-rbac.yaml
